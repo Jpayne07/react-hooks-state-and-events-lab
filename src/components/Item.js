b811d8c3ded5dc388fd9ac7c10f1f9793e4d0b7b
Jacob <jacobpayne007@gmail.com>
2024-07-12 14:05:42 -0500

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [state, stateSetter] = useState(false)
+  let className = state? 'in-cart': ""
+  let buttonText = state? 'Remove from Cart': "Add to Cart"
+  
+  function eHandler1(){
+    stateSetter((state)=>!state)
+  }
+
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick = {eHandler1}>{buttonText}</button>
     </li>
   );
 }
